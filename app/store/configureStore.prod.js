1d0cd59f680325d19298c734dde75160e8c44e91
Sundeep Paruchuri <Sundeep.Paruchuri@gmail.com>
2018-10-26 09:58:10 -0400

    restoring prod store config

--- app/store/configureStore.prod.js
+++ app/store/configureStore.prod.js
@@ -1,20 +1,18 @@
 // @flow
-import {
-  createStore
-  // , applyMiddleware
-} from 'redux';
-// import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { createHashHistory } from 'history';
-// import { routerMiddleware } from 'react-router-redux';
+import { routerMiddleware } from 'react-router-redux';
 import rootReducer from '../reducers';
 import type { appState } from '../reducers/types';
 
 const history = createHashHistory();
-// const router = routerMiddleware(history);
-// const enhancer = applyMiddleware(thunk, router);
+const router = routerMiddleware(history);
+const enhancer = applyMiddleware(thunk, router);
 
 function configureStore(initialState?: appState) {
-  return createStore(rootReducer, initialState);
+  // $FlowFixMe
+  return createStore(rootReducer, initialState, enhancer);
 }
 
 export default { configureStore, history };
